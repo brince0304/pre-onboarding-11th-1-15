73a4c5c2e19c62da4651dbe9352fecd1f0729045
윤선용 <sunyong9059@naver.com>
2023-06-29 02:02:52 +0900

    feat : input, 유효성 관리 state 생성

--- src/components/auths/AuthInput.tsx
+++ src/components/auths/AuthInput.tsx
@@ -1,8 +1,39 @@
+import React, { useState, useEffect } from "react"
+
 export default function AuthInput() {
+
+  interface User {
+    email: string
+    password: string
+  }
+
+  //input 값 보관
+  const [userInput, setUserInput] = useState<User>({
+    email: '',
+    password: ''
+  })
+
+  //유효성 검사
+  const [isEmailValied, setIsEmailValied] = useState({
+    msg: '',
+    validation: false
+  })
+  const [isPasswordValied, setIsPasswordvalied] = useState({
+    msg: '',
+    validation: false
+  })
   return (
     <div>
-      <input data-testid="email-input" name="email" />
-      <input data-testid="password-input" name="password" />
+      <input
+        value={userInput.email}
+        data-testid="email-input"
+        name="email"
+      />
+      <input
+        value={userInput.password}
+        data-testid="password-input"
+        name="password"
+      />
     </div>
   );
 }
