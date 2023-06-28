4b51d805d1dde201d031e6b57f8781ef4029ee54
JKKIM <jkesonjk@gmail.com>
2023-06-28 22:26:35 +0900

    feat() : baseurl 설정 및 todo 완료 로직 적용

--- src/components/todos/TodoItem.tsx
+++ src/components/todos/TodoItem.tsx
@@ -6,15 +6,50 @@ interface TodoItemProps {
   setTodos: React.Dispatch<React.SetStateAction<ITodo[]>>;
 }
 
+const BASE_URL = 'https://www.pre-onboarding-selection-task.shop';
+
 const TodoItem = ({ todo, setTodos }: TodoItemProps) => {
   const [isOnEdit, setIsOnEdit] = useState<boolean>(false);
   const [editTodoText, setEditTodoText] = useState<string>(todo.todo);
+
+  const handleComplete = async (todoItem: ITodo) => {
+    const updatedTodoItem = {
+      ...todoItem,
+      isCompleted: !todoItem.isCompleted,
+    };
+
+    const access_Token = localStorage.getItem('accessToken');
+
+    const response = await fetch(`${BASE_URL}/todos/${todoItem.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${access_Token}`,
+      },
+      body: JSON.stringify(updatedTodoItem),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      setTodos((todos) => todos?.map((todo) => (todo.id === todoItem.id ? data : todo)));
+    }
+
+    if (!response.ok) {
+      throw new Error(data.message || '업데이트에 문제가 발생했습니다.');
+    }
+  };
 
   return (
     <li>
       <div>
         <label>
-          <input type="checkbox" id={todo.id.toString()} checked={todo.isCompleted} />
+          <input
+            type="checkbox"
+            id={todo.id.toString()}
+            checked={todo.isCompleted}
+            onChange={() => handleComplete(todo)}
+          />
           {!isOnEdit ? (
             <span>{todo.todo}</span>
           ) : (
