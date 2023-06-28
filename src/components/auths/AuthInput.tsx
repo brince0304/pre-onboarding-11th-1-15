9d2938ab5840c5f7d7e7ee473ed143197e68447e
윤선용 <sunyong9059@naver.com>
2023-06-29 02:05:59 +0900

    feat : 유효성 검증 추가 및 onChangeHandler 추가

--- src/components/auths/AuthInput.tsx
+++ src/components/auths/AuthInput.tsx
@@ -1,36 +1,42 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from 'react';
 
 export default function AuthInput() {
-
   interface User {
-    email: string
-    password: string
+    email: string;
+    password: string;
   }
 
   //input 값 보관
   const [userInput, setUserInput] = useState<User>({
     email: '',
-    password: ''
-  })
+    password: '',
+  });
 
   //유효성 검사
   const [isEmailValied, setIsEmailValied] = useState({
     msg: '',
-    validation: false
-  })
+    validation: false,
+  });
   const [isPasswordValied, setIsPasswordvalied] = useState({
     msg: '',
-    validation: false
-  })
+    validation: false,
+  });
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setUserInput({ ...userInput, [name]: value })
+  }
   return (
     <div>
       <input
         value={userInput.email}
+        onChange={onChangeHandler}
         data-testid="email-input"
         name="email"
       />
       <input
         value={userInput.password}
+        onChange={onChangeHandler}
         data-testid="password-input"
         name="password"
       />
