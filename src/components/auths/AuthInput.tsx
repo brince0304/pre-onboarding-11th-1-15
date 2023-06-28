641ebc47823b07e6e8f19b8992ce66112e64de0d
윤선용 <sunyong9059@naver.com>
2023-06-29 01:59:52 +0900

    feat : AuthInput 컴포넌트 생성

---
+++ src/components/auths/AuthInput.tsx
@@ -0,0 +1,8 @@
+export default function AuthInput() {
+  return (
+    <div>
+      <input data-testid="email-input" name="email" />
+      <input data-testid="password-input" name="password" />
+    </div>
+  );
+}
