d186b18a66dc27ea581b369e319bee125bac6a4a
천진아 <totter15@naver.com>
2023-06-28 22:47:13 +0900

    feat(): auth api 함수 작성

---
+++ src/apis/auth.tsx
@@ -0,0 +1,16 @@
+import client from './client';
+
+interface AuthData {
+  email: string;
+  password: string;
+}
+
+export async function signUp({ email, password }: AuthData) {
+  const data = await client.post('auth/signup', { email, password });
+  return data;
+}
+
+export async function signIn({ email, password }: AuthData) {
+  const data = await client.post('auth/signin', { email, password });
+  return data;
+}
