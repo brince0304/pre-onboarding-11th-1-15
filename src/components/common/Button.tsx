f336f26e2d0fba79e808aba7683d3d0a994f9a21
seon-mikim <tjsalsun@gmail.com>
2023-06-28 23:06:20 +0900

    feat: Button 공통 컴포넌트 생성 및 구현
    - 프롭스 처리
    - interfaces 폴더 없음으로 해당 파일 작성
    - 사용 예시 주석 처리

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import { Sbutton } from './Button.style'
-
+import React from 'react';
+import { Sbutton } from './Button.style';
 
 export interface IButton extends Omit<React.HTMLProps<HTMLButtonElement>, 'size'> {
-    size: 'small' | 'medium' | 'large'
+  size: 'small' | 'medium' | 'large';
 }
 
-function Button({size, name, ...props}:IButton) {
+/**
+ *
+ *
+* import Button from 'components/common/Button';
+*
+* // 사용예시
+* // 함수는 따로 만드셔서 해당 onClick에 넣어 주시면 됩니다. :)
+* 
+*  <Button size='small' name="buttonTest" data-test='111' onClick={(e) => {
+        alert('hi')}}/>
+*
+*
+@returns JSX.Element
+*/
+
+function Button({ size, name, ...props }: IButton) {
   return (
-    <Sbutton size={size} {...props}>{name}</Sbutton>
-  )
+    <Sbutton size={size} {...props}>
+      {name}
+    </Sbutton>
+  );
 }
 
-export default Button
+export default Button;
