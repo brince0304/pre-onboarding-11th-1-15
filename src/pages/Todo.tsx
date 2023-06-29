14c5230451482150cc2fb7ea31fa35e2f3035509
JKKIM <jkesonjk@gmail.com>
2023-06-29 16:18:19 +0900

    fix(): useEffect 단일 역할로 수정 및 위치 조정

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -3,6 +3,8 @@ import { useNavigate } from 'react-router';
 import { ITodo } from 'interface/todoType';
 import { getTodos } from 'apis/todo';
 // import TodoInput from 'components/todos/TodoInput';
+// import TodoInput from 'components/todos/TodoList;
+import TodoList from 'components/todos/TEST_TodoList';
 
 const Todo = () => {
   const [todos, setTodos] = useState<ITodo[]>([]);
@@ -13,17 +15,6 @@ const Todo = () => {
     const data = await getTodos();
     setTodos(data);
   };
-
-  useEffect(() => {
-    const access_token = localStorage.getItem('access_token');
-
-    if (!access_token) {
-      navigate('/signin');
-      return;
-    }
-
-    getTodoList();
-  }, [navigate]);
 
   // todo 페이지 진입 상태에서 토큰이 소실될 경우 브라우저 객체에서 인식하여 바로 리다이렉트 처리
   useEffect(() => {
@@ -45,15 +36,14 @@ const Todo = () => {
     };
   }, [navigate]);
 
+  useEffect(() => {
+    getTodoList();
+  }, []);
+
   return (
     <>
       {/* <TodoInput getTodoList={getTodoList} /> */}
-      <ul>
-        {todos?.map((todo) => (
-          // <TodoList key={todo.id} />
-          <></>
-        ))}
-      </ul>
+      <TodoList todos={todos} setTodos={setTodos} />
     </>
   );
 };
