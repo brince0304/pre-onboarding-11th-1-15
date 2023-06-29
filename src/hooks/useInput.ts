02176d00ec1f39af34db6e25fc28586d47d9b976
brince0304 <brince0304@gmail.com>
2023-06-29 20:06:17 +0900

    fix: useInput 배열 반환하도록 수정
    - 객체형식으로 반환할시에 여러번 사용이 불가능할거같다고 판단하여 변경

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,50 +1,50 @@
-import React, { RefObject, useState } from 'react';
+import React, {RefObject, useState} from "react";
 
-interface IUseInputReturn {
-  onChange: React.ChangeEventHandler<HTMLInputElement>;
-  value: string;
-  isValidated: boolean;
-  setValue: React.Dispatch<React.SetStateAction<string>>;
-  setIsValidated: React.Dispatch<React.SetStateAction<boolean>>;
-  setFocus: () => void;
-  setBlur: () => void;
-}
+type IUseInputReturn = [
+    onChange: React.ChangeEventHandler<HTMLInputElement>,
+    value: any,
+    setValue: React.Dispatch<React.SetStateAction<string>>,
+    isValidated: boolean,
+    setIsValidated: React.Dispatch<React.SetStateAction<boolean>>,
+    setFocus: () => void,
+    setBlur: () => void,
+]
 
 const useInput = (options: {
-  regex: RegExp;
-  ref: RefObject<HTMLInputElement>;
-  initialValue?: string;
+    regex: RegExp;
+    ref: RefObject<HTMLInputElement>;
+    initialValue?: string;
 }): IUseInputReturn => {
-  const { regex } = options || {};
-  const [value, setValue] = useState<string>(options.initialValue || '');
-  const [isValidated, setIsValidated] = useState<boolean>(false);
+    const {regex} = options || {};
+    const [value, setValue] = useState<string>(options.initialValue || "");
+    const [isValidated, setIsValidated] = useState<boolean>(false);
 
-  const validateValue = (value: string) => {
-    const isValid = regex.test(value);
-    setIsValidated(isValid);
-  };
+    const validateValue = (value: string) => {
+        const isValid = regex.test(value);
+        setIsValidated(isValid);
+    };
 
-  const setFocus = () => {
-    if (options.ref) {
-      options.ref.current?.focus();
-    }
-  };
+    const setFocus = () => {
+        if (options.ref) {
+            options.ref.current?.focus();
+        }
+    };
 
-  const setBlur = () => {
-    if (options.ref) {
-      options.ref.current?.blur();
-    }
-  };
+    const setBlur = () => {
+        if (options.ref) {
+            options.ref.current?.blur();
+        }
+    };
 
-  const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    const value = e.target.value;
-    setValue(value);
-    validateValue(value);
-  };
+    const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        const value = e.target.value;
+        setValue(value);
+        validateValue(value);
+    };
 
-  /*  폼 제출시 유효성 검사 초기화, 값 초기화를 위해 setter 까지 반환하도록 했습니다.
-        전체적으로 빈 값이 들어가는 경우는 없기때문에 정규표현식으로 관리하도록 구현했습니다.*/
-  return { onChange, value, isValidated, setValue, setIsValidated, setFocus, setBlur };
+    /*  폼 제출시 유효성 검사 초기화, 값 초기화를 위해 setter 까지 반환하도록 했습니다.
+          전체적으로 빈 값이 들어가는 경우는 없기때문에 정규표현식으로 관리하도록 구현했습니다.*/
+    return [onChange, value, setValue, isValidated, setIsValidated, setFocus, setBlur];
 };
 
 export default useInput;
