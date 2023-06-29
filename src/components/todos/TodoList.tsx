652e8cfc235f8393a46d38a9fb568da3ac71bfb0
JangEunJeong <cuttlefish77777@gmail.com>
2023-06-29 16:42:37 +0900

    feat: TodoList 컴포넌트 생성 및 구현

---
+++ src/components/todos/TodoList.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ITodo } from 'apis/todo';
+import TodoItem from './TodoItem';
+
+interface TodoItemProps {
+  todos: ITodo[];
+  setTodos: React.Dispatch<React.SetStateAction<ITodo[]>>;
+}
+const TodoList = ({todos, setTodos} : TodoItemProps) => {
+  return (
+    <ul>
+      {todos.map((data: ITodo) => (
+        <TodoItem key={data.id} todo={data} setTodos={setTodos} />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
