b48061789cbf8fbe1fe7f17138394187a72ef76d
JKKIM <jkesonjk@gmail.com>
2023-06-30 00:57:17 +0900

    refactor(): useInput 적용 리팩토링

--- src/components/todos/TodoItem.tsx
+++ src/components/todos/TodoItem.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { ITodo } from 'interface/todoType';
 import { updateTodo, deleteTodo } from 'apis/todo';
 import Button from 'components/common/Button';
 import * as S from './TodoItem.style';
+import useInput from 'hooks/useInput';
+import Input, { IInputProps } from '../common/Input';
 
 interface TodoItemProps {
   todo: ITodo;
@@ -13,11 +15,31 @@ const TodoItem = ({ todo, setTodos }: TodoItemProps) => {
   const [isOnEdit, setIsOnEdit] = useState<boolean>(false);
   const [editTodoText, setEditTodoText] = useState<string>(todo.todo);
 
-  const handleDelete = async (todoItem: ITodo) => {
-    const res = await deleteTodo(todoItem.id);
+  const regex = /^.{1,}$/;
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { onChange, value, setValue, isValidated, setIsValidated, setFocus } = useInput({
+    regex,
+    ref: inputRef,
+    initialValue: editTodoText,
+  });
 
-    if (res) {
-      setTodos((todos) => todos?.filter((todo) => todo.id !== todoItem.id));
+  const InputProps = {
+    value,
+    onChange,
+    type: 'text',
+    dataTestId: 'modify-input',
+    ref: inputRef,
+  } as IInputProps;
+
+  const handleDelete = async (todoItem: ITodo) => {
+    try {
+      const resStatus = await deleteTodo(todoItem.id);
+
+      if (resStatus) {
+        setTodos((todos) => todos?.filter((todo) => todo.id !== todoItem.id));
+      }
+    } catch (error: any) {
+      throw new Error(error.response?.data.message || '정상적으로 삭제되지 않았습니다.');
     }
   };
 
@@ -27,19 +49,33 @@ const TodoItem = ({ todo, setTodos }: TodoItemProps) => {
   ) => {
     e.preventDefault();
 
-    if (editTodoText === todoItem.todo) {
-      setIsOnEdit((prev) => !prev);
+    setFocus();
+    // input text value가 비었는지 먼저 확인
+    if (isValidated) {
+      // 비어있지 않은 경우라도 기존과 같은 텍스트라면 붋필요한 api 요청않도록 취소하고 얼리 리턴으로 종료
+      if (editTodoText === todoItem.todo) {
+        setIsOnEdit((prev) => !prev);
+        return;
+      }
+
+      const editedTodoItem = {
+        id: todoItem.id.toString(),
+        todo: editTodoText,
+        isCompleted: todoItem.isCompleted,
+      };
+
+      try {
+        const resUpdatedTodo = await updateTodo(editedTodoItem);
+        setTodos((todos) => todos.map((todo) => (todo.id === todoItem.id ? resUpdatedTodo : todo)));
+        setValue('');
+      } catch (error: any) {
+        setIsValidated(false);
+        setFocus();
+        throw new Error(error.response?.data.message || '업데이트에 에러가 발생했습니다.');
+      }
+    } else {
       return;
     }
-
-    const editedTodoItem = {
-      id: todoItem.id.toString(),
-      todo: editTodoText,
-      isCompleted: todoItem.isCompleted,
-    };
-
-    const resUpdatedTodo = await updateTodo(editedTodoItem);
-    setTodos((todos) => todos.map((todo) => (todo.id === todoItem.id ? resUpdatedTodo : todo)));
   };
 
   const handleCancel = () => {
@@ -57,16 +93,7 @@ const TodoItem = ({ todo, setTodos }: TodoItemProps) => {
             checked={todo.isCompleted}
             onChange={(e) => handleUpdate(e, todo)}
           />
-          {!isOnEdit ? (
-            <span>{todo.todo}</span>
-          ) : (
-            <input
-              data-testid="modify-input"
-              type="text"
-              value={editTodoText}
-              onChange={(e) => setEditTodoText(e.target.value)}
-            />
-          )}
+          {!isOnEdit ? <span>{todo.todo}</span> : <Input {...InputProps} />}
         </S.Label>
         {!isOnEdit && (
           <>
