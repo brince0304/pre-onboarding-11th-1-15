664969cfb7ce89232e31283d6e9995ec617ce54c
천진아 <totter15@naver.com>
2023-06-29 14:03:24 +0900

    fix: interceptor response 추가

---
+++ src/apis/clientInterceptor.tsx
@@ -0,0 +1,24 @@
+import { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
+
+const onRequest = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+};
+
+const onResponse = (response: AxiosResponse): AxiosResponse => {
+  return response;
+};
+
+const onErrorResponse = (error: AxiosError): Promise<AxiosError> => {
+  if (error.response?.status === 401) {
+    localStorage.removeItem('token');
+  }
+
+  return Promise.reject(error);
+};
+
+export { onRequest, onResponse, onErrorResponse };
