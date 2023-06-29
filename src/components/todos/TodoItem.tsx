dc9ceb9b25237cf7a25eafd31e218d97fbd622a1
JKKIM <jkesonjk@gmail.com>
2023-06-29 17:30:58 +0900

    refactor(): todo update, delete api 로직 적용

--- src/components/todos/TodoItem.tsx
+++ src/components/todos/TodoItem.tsx
@@ -1,62 +1,28 @@
 import React, { useState } from 'react';
 import { ITodo } from 'interface/todoType';
-import axios from 'axios';
+import { updateTodo, deleteTodo } from 'apis/todo';
 
 interface TodoItemProps {
   todo: ITodo;
   setTodos: React.Dispatch<React.SetStateAction<ITodo[]>>;
 }
 
-const BASE_URL = 'https://www.pre-onboarding-selection-task.shop';
-
 const TodoItem = ({ todo, setTodos }: TodoItemProps) => {
   const [isOnEdit, setIsOnEdit] = useState<boolean>(false);
   const [editTodoText, setEditTodoText] = useState<string>(todo.todo);
 
-  const handleComplete = async (todoItem: ITodo) => {
-    const updatedTodoItem = {
-      ...todoItem,
-      isCompleted: !todoItem.isCompleted,
-    };
-
-    const access_Token = localStorage.getItem('accessToken');
-
-    try {
-      const response = await axios.put(`${BASE_URL}/todos/${todoItem.id}`, updatedTodoItem, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${access_Token}`,
-        },
-      });
-
-      if (response.status === 200) {
-        setTodos((todos) => todos?.map((todo) => (todo.id === todoItem.id ? response.data : todo)));
-      }
-    } catch (error: any) {
-      throw new Error(error.response.data.message || '업데이트에 에러가 발생했습니다.');
-    }
-  };
-
   const handleDelete = async (todoItem: ITodo) => {
-    const access_Token = localStorage.getItem('accessToken');
+    const res = await deleteTodo(todoItem.id);
 
-    try {
-      const response = await axios(`${BASE_URL}/todos/${todoItem.id}`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${access_Token}`,
-        },
-      });
-
-      if (response.status === 200) {
-        setTodos((todos) => todos?.filter((todo) => todo.id !== todoItem.id));
-      }
-    } catch (error: any) {
-      throw new Error(error.response.data.message || '정상적으로 삭제되지 않았습니다.');
+    if (res) {
+      setTodos((todos) => todos?.filter((todo) => todo.id !== todoItem.id));
     }
   };
 
-  const handleUpdate = async (e: React.FormEvent<HTMLButtonElement>, todoItem: ITodo) => {
+  const handleUpdate = async (
+    e: React.FormEvent<HTMLInputElement> | React.FormEvent<HTMLButtonElement>,
+    todoItem: ITodo,
+  ) => {
     e.preventDefault();
 
     if (editTodoText === todoItem.todo) {
@@ -65,26 +31,13 @@ const TodoItem = ({ todo, setTodos }: TodoItemProps) => {
     }
 
     const editedTodoItem = {
-      ...todoItem,
+      id: todoItem.id.toString(),
       todo: editTodoText,
+      isCompleted: todoItem.isCompleted,
     };
 
-    const access_token = localStorage.getItem('accessToken');
-
-    try {
-      const response = await axios.put(`${BASE_URL}/todos/${todoItem.id}`, editedTodoItem, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${access_token}`,
-        },
-      });
-
-      if (response.status === 200) {
-        setTodos((todos) => todos.map((todo) => (todo.id === todoItem.id ? response.data : todo)));
-      }
-    } catch (error: any) {
-      throw new Error(error.response.data.message || '정상적으로 업데이트되지 않았습니다.');
-    }
+    const resUpdatedTodo = await updateTodo(editedTodoItem);
+    setTodos((todos) => todos.map((todo) => (todo.id === todoItem.id ? resUpdatedTodo : todo)));
   };
 
   const handleCancel = () => {
@@ -100,7 +53,7 @@ const TodoItem = ({ todo, setTodos }: TodoItemProps) => {
             type="checkbox"
             id={todo.id.toString()}
             checked={todo.isCompleted}
-            onChange={() => handleComplete(todo)}
+            onChange={(e) => handleUpdate(e, todo)}
           />
           {!isOnEdit ? (
             <span>{todo.todo}</span>
