7f0930f379fa970586037b1df488a54d9343929e
천진아 <totter15@naver.com>
2023-06-30 20:53:07 +0900

    fix: Home 스타일링

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,14 @@
+import Header from 'components/Header';
+import * as S from './Home.style';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <S.Wrapper>
+      <Header />
+      <S.Title>Wanted Pre Onboarding Frontend 11th</S.Title>
+      <S.SubTitle>15팀</S.SubTitle>
+    </S.Wrapper>
+  );
 }
 
 export default Home;
